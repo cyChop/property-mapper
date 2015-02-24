13794361008dd713086427d0f5ce4d830294f819
Cyrille Chopelet <cyrille.chopelet@mines-nancy.org>
2015-02-24 19:29:17 +0100

    Removed the ASF 2.0 boilerplates after shifting license to BSD 3-clause

--- src/main/java/org/keyboardplaying/mapper/converter/BooleanConverter.java
+++ src/main/java/org/keyboardplaying/mapper/converter/BooleanConverter.java
@@ -1,23 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.keyboardplaying.mapper.converter;
 
-import org.keyboardplaying.mapper.exception.ConversionException;
 import org.keyboardplaying.mapper.Defaults;
+import org.keyboardplaying.mapper.exception.ConversionException;
 
 /**
  * Implementation of {@link Converter} for {@code Boolean} to {@code String} conversion.
@@ -46,7 +30,7 @@ public class BooleanConverter implements Converter<Boolean> {
 
     /*
      * (non-Javadoc)
-     *
+     * 
      * @see org.keyboardplaying.mapper.converter.Converter#convertFromString(java.lang.String)
      */
     @Override
@@ -66,7 +50,7 @@ public class BooleanConverter implements Converter<Boolean> {
 
     /*
      * (non-Javadoc)
-     *
+     * 
      * @see org.keyboardplaying.mapper.converter.Converter#convertToString(java.lang.Object)
      */
     @Override
--- src/test/java/org/keyboardplaying/mapper/engine/AutoDiscoverConverterProviderTest.java
+++ src/test/java/org/keyboardplaying/mapper/engine/AutoDiscoverConverterProviderTest.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.keyboardplaying.mapper.engine;
 
 import static org.junit.Assert.assertEquals;
@@ -33,7 +17,6 @@ import org.keyboardplaying.mapper.converter.CalendarConverter;
 import org.keyboardplaying.mapper.converter.IntegerConverter;
 import org.keyboardplaying.mapper.converter.LongConverter;
 import org.keyboardplaying.mapper.converter.StringConverter;
-import org.keyboardplaying.mapper.engine.AutoDiscoverConverterProvider;
 import org.keyboardplaying.mapper.exception.ConverterInitializationException;
 import org.keyboardplaying.mapper.mock.converter.DoubleConverter;
 import org.keyboardplaying.mapper.mock.converter.ExtendedDateConverter;
